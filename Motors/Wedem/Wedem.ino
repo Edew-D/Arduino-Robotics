802835307b81196ec7ca14df9deb65eb8f9916a4
antikz23 <jslizzz30@gmail.com>
2016-12-15 15:19:06 -0500

    edited

--- Motors/Wedem/Wedem.ino
+++ Motors/Wedem/Wedem.ino
@@ -1,10 +1,19 @@
-#define   LEFT_MOTOR_EN   12
-#define   lb   6   
-#define   lr   7
-#define   RIGHT_MOTOR_EN   8
-#define   rr   9
-#define   rb   10
-void forward()
+#define   LEFT_MOTOR_EN   8
+#define   lb   7   
+#define   lr   6
+#define   RIGHT_MOTOR_EN   11
+#define   rr   10
+#define   rb   9
+void right()
+{
+
+	digitalWrite(lr, HIGH);
+	digitalWrite(lb, LOW);
+	digitalWrite(rb, LOW);
+	digitalWrite(rr, HIGH);
+}
+
+void left()
 {
 
 	digitalWrite(lr, LOW);
@@ -12,47 +21,48 @@ void forward()
 	digitalWrite(rb, HIGH);
 	digitalWrite(rr, LOW);
 }
-void sqt()
-{
-	digitalWrite(lb, LOW);
-	digitalWrite(lr, HIGH);
-	digitalWrite(rr, LOW);
-	digitalWrite(rb, HIGH);
-}
-void right()
-{
-	digitalWrite(lb, LOW);
-	digitalWrite(lr, HIGH);
-	digitalWrite(rr, LOW);
-	digitalWrite(rb, HIGH);
-}
-void Stay()
+void fwd()
 {
 	digitalWrite(lb, HIGH);
+	digitalWrite(lr, LOW);
+	digitalWrite(rr, HIGH);
+	digitalWrite(rb, LOW);
+}
+void back()
+{
+	digitalWrite(lb, LOW);
 	digitalWrite(lr, HIGH);
+	digitalWrite(rr, LOW);
+	digitalWrite(rb, HIGH);
+}
+void stay()
+{
+	digitalWrite(lb, HIGH);
+	digitalWrite(lr, HIGH); 
 	digitalWrite(rr, HIGH);
 	digitalWrite(rb, HIGH);
 }
+
+void t90r()
+{ 
+  right();
+delay(740);
+stay();
+
+}
+
 void setup()
 {
- for (int x = 5; x <= 12; x++)
+ for (int x = 5; x <= 12; x++) 
   { 
-    digitalWrite(x, HIGH);
+    pinMode(x, HIGH);
   }
   	digitalWrite(LEFT_MOTOR_EN, HIGH);
   	digitalWrite(RIGHT_MOTOR_EN, HIGH);
 }
 void loop()
 {
-forward();
-delay(4000);
-Stay();
-delay(1000);
-right();
-delay(4000);
-sqt();
-delay(1000);
-
-
-
+ t90r();
+ fwd();
+ delay(2500);
 }
