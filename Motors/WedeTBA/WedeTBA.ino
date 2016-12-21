b9a225193f5c64d874318f20254b3716da5f7ad4
antikz23 <jslizzz30@gmail.com>
2016-12-21 14:02:07 -0500

    Updated

--- Motors/WedeTBA/WedeTBA.ino
+++ Motors/WedeTBA/WedeTBA.ino
@@ -10,78 +10,79 @@ int s;
 void right()
 {
 
-	digitalWrite(lr, LOW);
-	digitalWrite(lb, LOW);
-	digitalWrite(rb, LOW );
-	digitalWrite(rr, HIGH);
+  digitalWrite(lr, LOW);
+  digitalWrite(lb, LOW);
+  digitalWrite(rb, LOW );
+  digitalWrite(rr, HIGH);
 }
 
 void left()
 {
 
-	digitalWrite(lr, LOW);
-	digitalWrite(lb, HIGH);
-	digitalWrite(rb, HIGH);
-	digitalWrite(rr, LOW);
+  digitalWrite(lr, LOW);
+  digitalWrite(lb, HIGH);
+  digitalWrite(rb, HIGH);
+  digitalWrite(rr, LOW);
 }
 void fwd()
 {
-	digitalWrite(lb, HIGH);
-	digitalWrite(lr, LOW);
-	digitalWrite(rr, HIGH);
-	digitalWrite(rb, LOW);
+  digitalWrite(lb, HIGH);
+  digitalWrite(lr, LOW);
+  digitalWrite(rr, HIGH);
+  digitalWrite(rb, LOW);
 }
 void back()
 {
-	digitalWrite(lb, LOW);
-	digitalWrite(lr, HIGH);
-	digitalWrite(rr, LOW);
-	digitalWrite(rb, HIGH);
+  digitalWrite(lb, LOW);
+  digitalWrite(lr, HIGH);
+  digitalWrite(rr, LOW);
+  digitalWrite(rb, HIGH);
 }
 void stay()
 {
-	digitalWrite(lb, HIGH);
-	digitalWrite(lr, HIGH); 
-	digitalWrite(rr, HIGH);
-	digitalWrite(rb, HIGH);
+  digitalWrite(lb, HIGH);
+  digitalWrite(lr, HIGH); 
+  digitalWrite(rr, HIGH);
+  digitalWrite(rb, HIGH);
 }
 
 void t90r()
 { 
   right();
-delay(2870);
-stay();
+  delay(2870);
+  stay();
 
 }
 
 void square()
 { 
   right();
-delay(1385);
-stay();
+  delay(1385);
+  stay();
 
 }
 
 
 void setup()
 {
- for (int x = 6; x <= 12; x++) 
+  for (int x = 6; x <= 12; x++) 
   { 
     pinMode(x, OUTPUT);
   }
-  	digitalWrite(LEFT_MOTOR_EN, HIGH);
-  	digitalWrite(RIGHT_MOTOR_EN, HIGH);
-        pinMode(5, INPUT);
+  digitalWrite(LEFT_MOTOR_EN, HIGH);
+  digitalWrite(RIGHT_MOTOR_EN, HIGH);0
+  pinMode(5, INPUT);
 }
 void loop()
 {
-s = digitalRead(5);
-if (s == HIGH)
-{ 
-  fwd();
+  s = digitalRead(5);
+  if (s == HIGH)
+  { 
+    fwd();
+  }
+  else;
+  {
+    t90r();
+  }
 }
-else;
-{
-  t90r();
-}
-}
+
